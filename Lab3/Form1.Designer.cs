4dd509ffcfd9e74fde498167deaa13c838aba0bf
dravyman <drav10@rambler.ru>
2016-12-01 14:59:22 +0300

    Поправил размер.

--- Lab3/Form1.Designer.cs
+++ Lab3/Form1.Designer.cs
@@ -166,7 +166,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(495, 323);
+            this.ClientSize = new System.Drawing.Size(296, 83);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
