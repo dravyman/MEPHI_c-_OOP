a37d73e97623cdcfeff8ff4a23fd3f9ffdb06903
dravyman <drav10@rambler.ru>
2016-12-01 14:52:58 +0300

    Добавил проект.
    Создал класс Counter16
    Создал форму

---
+++ Lab3/Counter16.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Counter16
+    {
+        private int value10, max, min;
+        private string value16;
+
+        public int Min
+        {
+            get { return min; }
+        }
+        public int Max
+        {
+            get { return max; }
+        }
+        public int Value10
+        {
+            get { return value10; }
+            set { value10 = value; }
+        }
+        public string Value16
+        {
+            get { return Convert.ToString(value10,16); }
+        }
+
+        public Counter16()
+        {
+            this.min = 0;
+            this.max = 16;
+            this.value10 = 0;
+        }
+
+        public Counter16(int min, int max, int value10)
+        {
+            this.min = min;
+            this.max = max;
+            if (value10 > max || value10 < min)
+                throw (value10 > max) ? new Exception("Число выше чем максимум") : new Exception("Число ниже чем минимум");
+            this.value10 = value10;
+        }
+
+        public Counter16(int min, int max, string value16)
+        {
+            this.min = min;
+            this.max = max;
+            this.value10 = Convert.ToInt32(value16, 16);
+            if (value10 > max || value10 < min)
+                throw (value10 > max) ? new Exception("Число выше чем максимум") : new Exception("Число ниже чем минимум");
+        }
+
+        public void incValue()
+        {
+            if (value10 + 1 > max)
+                throw new Exception("Число выше чем максимум");
+            this.value10++;
+        }
+        public void decValue()
+        {
+            if (value10 - 1< min)
+                throw new Exception("Число ниже чем минимум");
+            this.value10--;
+        }
+
+    }
+}
---
+++ Lab3/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        Counter16 count;
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            count = new Counter16();
+            setValuesOnForm();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                count = new Counter16(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text));
+                setValuesOnForm();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int min = rnd.Next(0, 100);
+            int def = rnd.Next(0,10000);
+            int val = rnd.Next(min, min + def);
+            try
+            {
+                count = new Counter16(min, min+def, val);
+                setValuesOnForm();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                count.decValue();
+                setValuesOnForm();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                count.incValue();
+                setValuesOnForm();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void setValuesOnForm()
+        {
+            textBox1.Text = count.Min.ToString();
+            textBox2.Text = count.Max.ToString();
+            textBox3.Text = count.Value10.ToString();
+            textBox4.Text = count.Value16.ToString();
+        }
+
+    }
+}
