36c87c7b9b37dfb9c21abf05ceea750646373a52
dravyman <drav10@rambler.ru>
2016-12-01 18:33:34 +0300

    Добавлена картинка и PictureBox на форму

--- Program8/Form1.cs
+++ Program8/Form1.cs
@@ -12,9 +12,20 @@ namespace Program8
 {
     public partial class Form1 : Form
     {
+        Bitmap image1;
+        PictureBox pictureBox1;
+
         public Form1()
         {
-            InitializeComponent();
+            this.Text = "Искусство аборигенов";
+            this.Size = new Size(302, 240);
+            pictureBox1 = new PictureBox();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox1.ClientSize = new Size(300, 196);
+            image1 = new Bitmap(@"../../images/Desert.jpg");
+            pictureBox1.Image = (Image)image1;
+            this.Controls.Add(pictureBox1);
         }
     }
 }
