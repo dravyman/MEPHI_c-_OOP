c11515680ef771554912dba8cedc411b48cbb6e9
dravyman <drav10@rambler.ru>
2016-12-02 13:50:57 +0300

    Добавлено зеркальное отражение картинки.

--- Program9/Form1.cs
+++ Program9/Form1.cs
@@ -12,9 +12,38 @@ namespace Program9
 {
     public partial class Form1 : Form
     {
+        Button button1;
+        Bitmap image1;
+        PictureBox pictureBox1;
+
         public Form1()
         {
-            InitializeComponent();
+            this.Text = "Поворот рисунка";
+            this.Size = new Size(302, 240);
+
+            button1 = new Button();
+            button1.Text = "Поворот рисунка";
+            button1.Location = new Point(100, 150);
+            button1.Size = new Size(70, 40);
+            button1.Click += new System.EventHandler(button1_Click);
+            this.Controls.Add(button1);
+
+            pictureBox1 = new PictureBox();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox1.ClientSize = new Size(300, 196);
+
+            image1 = new Bitmap(@"../../images/Desert.jpg");
+            pictureBox1.Image = (Image)image1;
+
+            this.Controls.Add(pictureBox1);
+        }
+
+        void button1_Click(object sender, EventArgs e)
+        {
+            image1.RotateFlip(RotateFlipType.RotateNoneFlipX);
+            pictureBox1.Image = (Image)image1;
+            this.Text = "Рисунок после поворота!";
         }
     }
 }
